0c4e00e91dd194872db287c468454786aa5cde5c
nyj001012 <nyj001012@gmail.com>
2025-05-22 19:54:37 +0900

    feat: 알파 채널 지원하는 레이블 추가

---
+++ ScoreBoard/ScoreBoard/controls/TransparentTextLabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace ScoreBoard.controls
+{
+    public class TransparentTextLabel : Label
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rc = this.ClientRectangle;
+            StringFormat fmt = new StringFormat(StringFormat.GenericTypographic);
+            using (var br = new SolidBrush(this.ForeColor))
+            {
+                e.Graphics.DrawString(this.Text, this.Font, br, rc, fmt);
+            }
+        }
+    }
+}
