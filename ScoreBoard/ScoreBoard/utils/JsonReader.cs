7f2750daf83b5abf1554cad8dfbe06cdbbbac014
nyj001012 <nyj001012@gmail.com>
2025-05-22 17:30:27 +0900

    feat: json 파일 읽어오는 정적 클래스 생성

---
+++ ScoreBoard/ScoreBoard/utils/JsonReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.utils
+{
+    public static class JsonReader
+    {
+        public static Dictionary<string, string> ReadJsonStringValue(string jsonPath)
+        {
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader(jsonPath))
+                {
+                    string json = reader.ReadToEnd();
+                    var deserializedMap = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+                    if (deserializedMap != null)
+                    {
+                        map = deserializedMap;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading JSON file: {ex.Message}");
+            }
+            return map;
+        }
+    }
+}
