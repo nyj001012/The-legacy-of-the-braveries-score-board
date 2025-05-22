c9c30e266b1b0b77d5d9e2eb935e2abca3482850
nyj001012 <nyj001012@gmail.com>
2025-05-22 21:06:52 +0900

    feat: 더블 버퍼 지원하는 flowlayoutpanel 추가

---
+++ ScoreBoard/ScoreBoard/controls/CustomFlowLayoutPanel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.controls
+{
+    public class CustomFlowLayoutPanel : FlowLayoutPanel
+    {
+        public CustomFlowLayoutPanel()
+        {
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw
+                        | ControlStyles.UserPaint
+                        | ControlStyles.AllPaintingInWmPaint
+                        | ControlStyles.SupportsTransparentBackColor, true);
+        }
+
+        protected override void OnScroll(ScrollEventArgs se)
+        {
+            base.OnScroll(se);
+            this.Invalidate(); // 강제 다시 그리기
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+                return cp;
+            }
+        }
+    }
+}
