03803a520b25ac7528b6f430273574868e9e3363
nyj001012 <nyj001012@gmail.com>
2025-05-27 15:33:12 +0900

    feat: 인수 없는 생성자 사용

--- ScoreBoard/ScoreBoard/data/ActiveSkill.cs
+++ ScoreBoard/ScoreBoard/data/ActiveSkill.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ScoreBoard.data
 {
     internal class ActiveSkill : Skill
     {
-        public ushort Cooldown { get; private set; }
-        public ActiveSkill(string name, ushort level, string[] description, ushort cooldown) : base(name, level, description)
+        public ushort Cooldown { get; set; } = 3; // 기술 사용 후 재사용 대기 시간 (예: 3턴 후 재사용 가능)
+
+        public ActiveSkill() : base("", 0, [])
         {
-            Cooldown = cooldown; // 기술 사용 후 재사용 대기 시간 (예: 3턴 후 재사용 가능)
         }
 
-        public void SetCooldown(ushort cooldown)
-        {
-            Cooldown = cooldown; // 재사용 대기 시간 설정
-        }
+        [JsonIgnore]
+        public new Action? Execute { get; set; }
     }
 }
--- ScoreBoard/ScoreBoard/data/PassiveSkill.cs
+++ ScoreBoard/ScoreBoard/data/PassiveSkill.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ScoreBoard.data
 {
     internal class PassiveSkill : Skill
     {
-        public ushort Duration { get; private set; } = 0; // 지속 시간 (예: 2턴 동안 효과 지속). 0인 경우 영구 효과를 의미합니다.
-        public PassiveSkill(string name, ushort level, string[] description, ushort duration) : base(name, level, description)
+        public bool isActivated { get; set; } = false; // 지속 시간 (예: 2턴 동안 효과 지속). 0인 경우 영구 효과를 의미합니다.
+
+        public PassiveSkill() : base("", 0, [])
         {
-            Duration = duration; // 기술 효과 지속 시간 설정
         }
 
-        public void SetDuration(ushort duration)
-        {
-            Duration = duration; // 지속 시간 설정
-        }
+        [JsonIgnore]
+        public new Action? Execute { get; set; }
     }
 }
