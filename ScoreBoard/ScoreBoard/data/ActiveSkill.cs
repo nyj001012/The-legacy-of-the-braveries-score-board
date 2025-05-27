e7491d202c4e20325ee1076e4b42c1aee3effe23
nyj001012 <nyj001012@gmail.com>
2025-05-27 14:44:35 +0900

    feat: 누락된 부분 수정 및 ushort null대신 0으로 처리

--- ScoreBoard/ScoreBoard/data/ActiveSkill.cs
+++ ScoreBoard/ScoreBoard/data/ActiveSkill.cs
@@ -9,7 +9,7 @@ namespace ScoreBoard.data
     internal class ActiveSkill : Skill
     {
         public ushort Cooldown { get; private set; }
-        public ActiveSkill(string name, ushort level, string description, ushort cooldown) : base(name, level, description)
+        public ActiveSkill(string name, ushort level, string[] description, ushort cooldown) : base(name, level, description)
         {
             Cooldown = cooldown; // 기술 사용 후 재사용 대기 시간 (예: 3턴 후 재사용 가능)
         }
--- ScoreBoard/ScoreBoard/data/PassiveSkill.cs
+++ ScoreBoard/ScoreBoard/data/PassiveSkill.cs
@@ -8,8 +8,8 @@ namespace ScoreBoard.data
 {
     internal class PassiveSkill : Skill
     {
-        public ushort? Duration { get; private set; } // 지속 시간 (예: 2턴 동안 효과 지속). Null인 경우 영구 효과를 의미합니다.
-        public PassiveSkill(string name, ushort level, string description, ushort duration) : base(name, level, description)
+        public ushort Duration { get; private set; } = 0; // 지속 시간 (예: 2턴 동안 효과 지속). 0인 경우 영구 효과를 의미합니다.
+        public PassiveSkill(string name, ushort level, string[] description, ushort duration) : base(name, level, description)
         {
             Duration = duration; // 기술 효과 지속 시간 설정
         }
