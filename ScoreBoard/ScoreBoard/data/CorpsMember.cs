3c2c286249de02d41640b65772a5c25fdf5ec624
nyj001012 <nyj001012@gmail.com>
2025-05-27 14:45:40 +0900

    feat: 접근 한정자 수정 및 누락된 부분 추가

--- ScoreBoard/ScoreBoard/data/CorpsMember.cs
+++ ScoreBoard/ScoreBoard/data/CorpsMember.cs
@@ -8,44 +8,28 @@ namespace ScoreBoard.data
 {
     internal class CorpsMember(string id, string name, string corpsId, string[] description, Stat stat)
     {
-        protected string Id { get; } = id; // 멤버 ID: [군단 ID]_[번호]_이름
-        protected string Name { get; } = name; // 멤버 이름 (예: 루다, 스카이하늘소라텐등)
-        protected string CorpsId { get; } = corpsId; // 소속 군단 ID (예: "201", "000" 등)
-        protected string[] Description { get; } = description; // 배경 설명
-        protected Stat Stat { get; } = stat; // 멤버의 능력치 정보
+        public string Id { get; set; } = id; // 멤버 ID: [군단 ID]_[번호]_이름
+        public string Name { get; set; } = name; // 멤버 이름 (예: 루다, 스카이하늘소라텐등)
+        public string CorpsId { get; set; } = corpsId; // 소속 군단 ID (예: "201", "000" 등)
+        public string[] Description { get; set; } = description; // 배경 설명
+        public Stat Stat { get; set; } = stat; // 멤버의 능력치 정보
+        public List<PassiveSkill> Passives { get; set; } = []; // 멤버의 패시브 스킬 정보
+        public List<ActiveSkill> Actives { get; set; } = []; // 멤버의 능력치 정보
     }
 
-    internal class Stat(ushort movement, List<CombatStat> combatStats)
+    internal class Stat(ushort hp, ushort movement, ushort wisdom, Dictionary<string, CombatStat> combatStats)
     {
-        public ushort Movement { get; private set; } = movement; // 이동 가능 거리 (예: 1, 2, 3 등)
-        public List<CombatStat> CombatStats { get; } = combatStats; // 공격 타입별 공격력 정보 배열 (예: 물리(atk), 마법(sp) 등)
-
-        public void SetMovement(ushort movement)
-        {
-            Movement = movement;
-        }
+        public ushort Hp { get; set; } = hp; // 체력 (예: 10, 20 등)
+        public ushort Movement { get; set; } = movement; // 이동 가능 거리 (예: 1, 2, 3 등)
+        public ushort Wisdom { get; set; } = wisdom; // 지혜 = 카드 뽑는 횟수 (예: 1, 2, 3 등) - 선택적 속성
+        public Dictionary<string, CombatStat> CombatStats { get; set; } = combatStats; // 공격 타입별 공격력 정보 배열 (예: 물리(atk), 마법(sp) 등)
     }
 
     internal class CombatStat(string type, ushort range, ushort attackCount, ushort value)
     {
-        public string Type { get; } = type; // 공격 타입 (예: 물리(atk), 마법(sp))
-        public ushort Range { get; private set; } = range; // 공격 사거리 (예: 1, 2, 3 등)
-        public ushort AttackCount { get; private set; } = attackCount; // 공격 횟수 = 굴릴 수 있는 주사위 개수 (예: 1, 2, 3 등)
-        public ushort Value { get; private set; } = value; // 공격력 (예: 10, 20 등)
-
-        public void SetRange(ushort range)
-        {
-            Range = range;
-        }
-
-        public void SetAttackCount(ushort attackCount)
-        {
-            AttackCount = attackCount;
-        }
-
-        public void SetValue(ushort value)
-        {
-            Value = value;
-        }
+        public string Type { get; set; } = type; // 공격 타입 (예: 물리(atk), 마법(sp))
+        public ushort Range { get; set; } = range; // 공격 사거리 (예: 1, 2, 3 등)
+        public ushort AttackCount { get; set; } = attackCount; // 공격 횟수 = 굴릴 수 있는 주사위 개수 (예: 1, 2, 3 등)
+        public ushort Value { get; set; } = value; // 공격력 (예: 10, 20 등)
     }
 }
