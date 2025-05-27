c72f8d3b13315c5be8fac21838673001d06aea5f
nyj001012 <nyj001012@gmail.com>
2025-05-27 12:48:52 +0900

    feat: 캐릭터의 베이스 클래스 생성

--- ScoreBoard/ScoreBoard/data/CorpsMember.cs
+++ ScoreBoard/ScoreBoard/data/CorpsMember.cs
@@ -6,35 +6,26 @@ using System.Threading.Tasks;
 
 namespace ScoreBoard.data
 {
-    internal class CorpsMember
+    internal class CorpsMember(string id, string name, string corpsId, string[] description, Stat stat)
     {
-        private string id;
-        private string name;
-        private string corpsId;
-        private string[] description;
-        private Stat stat;
+        protected string Id { get; set; } = id; // 멤버 ID: [군단 ID]_[번호]_이름
+        protected string Name { get; set; } = name; // 멤버 이름 (예: 루다, 스카이하늘소라텐등)
+        protected string CorpsId { get; set; } = corpsId; // 소속 군단 ID (예: "201", "000" 등)
+        protected string[] Description { get; set; } = description; // 배경 설명
+        protected Stat Stat { get; set; } = stat; // 멤버의 능력치 정보
     }
 
-    internal class Stat
+    internal class Stat(ushort movement, CombatStat[] combatStat)
     {
-        private ushort hp;
-        private ushort movement;
-        private CombatStat[] combatStat;
+        private ushort Movement { get; set; } = movement; // 이동 가능 거리 (예: 1, 2, 3 등)
+        private CombatStat[] CombatStat { get; set; } = combatStat; // 공격 타입별 공격력 정보 배열 (예: 물리(atk), 마법(sp) 등)
     }
 
-    internal class CombatStat
+    internal class CombatStat(string type, ushort range, ushort attackCount, ushort value)
     {
-        private string Type { get; set; } // 공격 종류 (예: 물리(atk), 마법(sp))
-        private ushort Range { get; set; } // 공격 사거리 (예: 1, 2, 3 등)
-        private ushort AttackCount { get; set; } // 공격 횟수 = 굴릴 수 있는 주사위 개수 (예: 1, 2, 3 등)
-        private ushort Value { get; set; } // 공격력 및 주문력 (예: 1, 2, 3 등)
-
-        public CombatStat(string type, ushort range, ushort attackCount, ushort value)
-        {
-            this.Type = type;
-            this.Range = range;
-            this.AttackCount = attackCount;
-            this.Value = value;
-        }
+        private string Type { get; set; } = type; // 공격 타입 (예: 물리(atk), 마법(sp))
+        private ushort Range { get; set; } = range; // 공격 사거리 (예: 1, 2, 3 등)
+        private ushort AttackCount { get; set; } = attackCount; // 공격 횟수 = 굴릴 수 있는 주사위 개수 (예: 1, 2, 3 등)
+        private ushort Value { get; set; } = value; // 공격력 (예: 10, 20 등)
     }
 }
