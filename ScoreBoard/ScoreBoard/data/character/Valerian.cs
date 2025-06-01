7f32c87264b6a300a95460126d10ce736b1a866a
nyj001012 <nyj001012@gmail.com>
2025-06-01 12:50:43 +0900

    feat: 발레리안 기본 내용 추가

---
+++ ScoreBoard/ScoreBoard/data/character/Valerian.cs
@@ -0,0 +1,104 @@
+﻿using ScoreBoard.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ScoreBoard.data.character
+{
+    internal class Valerian : CorpsMember
+    {
+        public Valerian(string id)
+        {
+            Validator.ValidateNull(id, nameof(id));
+            var data = DataReader.ReadMemberData(id) ?? throw new ArgumentException($"데이터 불러오기 오류: {id}");
+            // 필드 초기화
+            InitialiseBasicInfo(data);
+            // Stat 초기화
+            InitialiseStat(data.Stat);
+            // 스킬 초기화
+            InitialisePasssiveSkills(data);
+            InitialiseActiveSkills(data);
+        }
+
+        private void InitialiseBasicInfo(CorpsMember data)
+        {
+            Id = Validator.ValidateNull(data.Id, nameof(data.Id));
+            Name = Validator.ValidateNull(data.Name, nameof(data.Name));
+            CorpsId = Validator.ValidateNull(data.CorpsId, nameof(data.CorpsId));
+            Description = Validator.ValidateNull(data.Description, nameof(data.Description));
+        }
+
+        private void InitialiseStat(Stat statData)
+        {
+            Validator.ValidateNull(statData, nameof(statData));
+            Validator.ValidateNull(statData.CombatStats, nameof(statData.CombatStats));
+            Stat = new Stat
+            {
+                Hp = statData.Hp,
+                MaxHp = statData.Hp, // 시작 시, 현재 체력은 최대 체력
+                Movement = statData.Movement,
+                Wisdom = statData.Wisdom, // nullable 또는 기본값 처리
+                CombatStats = statData.CombatStats.ToDictionary(
+                    kv => kv.Key,
+                    kv => new CombatStat
+                    {
+                        Type = kv.Value.Type,
+                        Range = kv.Value.Range,
+                        AttackCount = kv.Value.AttackCount,
+                        Value = kv.Value.Value
+                    }
+                ) ?? new Dictionary<string, CombatStat>()
+            };
+        }
+
+        private void InitialisePasssiveSkills(CorpsMember data)
+        {
+            Validator.ValidateNull(data.Passives, nameof(data.Passives));
+            Passives = data.Passives?.Select(p =>
+            {
+                var skill = new PassiveSkill()
+                {
+                    Name = Validator.ValidateNull(p.Name, nameof(p.Name)),
+                    RequiredLevel = p.RequiredLevel,
+                    Description = Validator.ValidateNull(p.Description, nameof(p.Description))
+                };
+
+                skill.Execute = p.Name switch
+                {
+                    "죽음의 천사" => () => skill.isActivated = true,
+                    "백수지만 능력자!!" => () => skill.isActivated = true,
+                    "백수가 되었지만 훈련을 게을리하지않아ㅜㅠㅠㅠㅠ으어헝" => () => TrainHard(),
+                    "백수가 되어도 굳건한 정신력 엉어유ㅠㅡㅠㅠㅠ" => () => FortifyMind(),
+                    _ => null
+                };
+                return skill;
+            }).ToList() ?? [];
+        }
+
+        private void InitialiseActiveSkills(CorpsMember data)
+        {
+            Validator.ValidateNull(data.Actives, nameof(data.Actives));
+            Actives = data.Actives?.Select(a =>
+            {
+                var skill = new ActiveSkill()
+                {
+                    Name = Validator.ValidateNull(a.Name, nameof(a.Name)),
+                    RequiredLevel = a.RequiredLevel,
+                    Description = Validator.ValidateNull(a.Description, nameof(a.Description)),
+                    Cooldown = a.Cooldown,
+                    isOnCooldown = false
+                };
+
+                skill.Execute = a.Name switch
+                {
+                    "만능이란 이런 것" => () => skill.isOnCooldown = true,
+                    _ => null
+                };
+                return skill;
+            }).ToList() ?? [];
+        }
+    }
+}
