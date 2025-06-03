307b75989101ab20110aae5032004ef5f825f53b
nyj001012 <nyj001012@gmail.com>
2025-06-03 15:21:11 +0900

    feat: 보스 몬스터는 엘리트 몹을 소환할 수 있게 함

--- ScoreBoard/ScoreBoard/data/monster/Monster.cs
+++ ScoreBoard/ScoreBoard/data/monster/Monster.cs
@@ -8,10 +8,12 @@ namespace ScoreBoard.data.monster
 {
     internal class Monster
     {
-        public ushort Id { get; init; }
-        public required string Name { get; init; }
-        public required Stat Stat { get; init;}
-        public required ushort[] AttackDiceValue { get; init; }
-        public required ushort SpawnTurn { get; init; }
+        public ushort Type { get; init; } // 몬스터 등급 (예: 0 보스, 1 엘리트, 2 일반).
+        public ushort Id { get; init; } // Id
+        public required string Name { get; init; } // 이름
+        public required Stat Stat { get; init;} // 스탯
+        public required ushort[] AttackDiceValue { get; init; } // 공격이 유효한 주사위 숫자
+        public required ushort SpawnTurn { get; init; } // 스폰 가능한 턴
+        public Skill? SpawnElites { get; set; } // 보스 몬스터는 엘리트 몬스터를 소환할 수 있음
     }
 }
