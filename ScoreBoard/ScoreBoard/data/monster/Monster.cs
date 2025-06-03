c541a8e0980834abc06601bd136374b9ac213ece
nyj001012 <nyj001012@gmail.com>
2025-06-03 14:50:17 +0900

    feat: 몬스터 베이스 클래스 추가

---
+++ ScoreBoard/ScoreBoard/data/monster/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data.monster
+{
+    internal class Monster
+    {
+        public ushort Id { get; private set; }
+        public required string Name { get; init; }
+        public required Stat Stat { get; init;}
+        public required ushort[] AttackDiceValue { get; init; }
+    }
+}
