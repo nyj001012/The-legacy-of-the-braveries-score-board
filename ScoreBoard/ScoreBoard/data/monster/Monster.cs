d2c10d0d13304b20755fc46d83943d904e7274e7
nyj001012 <nyj001012@gmail.com>
2025-06-03 15:29:21 +0900

    feat: 일반, 엘리트 몬스터의 초기화 함수 통합

--- ScoreBoard/ScoreBoard/data/monster/Monster.cs
+++ ScoreBoard/ScoreBoard/data/monster/Monster.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScoreBoard.utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,41 @@ namespace ScoreBoard.data.monster
 {
     internal class Monster
     {
-        public ushort Type { get; init; } // 몬스터 등급 (예: 0 보스, 1 엘리트, 2 일반).
-        public ushort Id { get; init; } // Id
-        public required string Name { get; init; } // 이름
-        public required Stat Stat { get; init;} // 스탯
-        public required ushort[] AttackDiceValue { get; init; } // 공격이 유효한 주사위 숫자
-        public required ushort SpawnTurn { get; init; } // 스폰 가능한 턴
+        public ushort Type { get; private set; } // 몬스터 등급 (예: 0 보스, 1 엘리트, 2 일반).
+        public ushort Id { get; private set; } // Id
+        public string Name { get; private set; } = string.Empty!; // 이름
+        public Stat Stat { get; private set; } = default!; // 스탯
+        public ushort[] AttackDiceValue { get; private set; } = default!; // 공격이 유효한 주사위 숫자
+        public ushort SpawnTurn { get; private set; } // 스폰 가능한 턴
         public Skill? SpawnElites { get; set; } // 보스 몬스터는 엘리트 몬스터를 소환할 수 있음
+
+        protected void InitialiseNormalElite(string id, ushort spawnTurn)
+        {
+            Validator.ValidateNull(id, nameof(id));
+
+            var data = DataReader.ReadMonsterData(id) ?? throw new ArgumentException($"데이터 불러오기 오류: {id}");
+
+            this.Id = data.Id;
+            this.Type = data.Type;
+            this.Name = data.Name;
+            this.AttackDiceValue = data.AttackDiceValue;
+            this.SpawnTurn = spawnTurn;
+            this.Stat = new Stat
+            {
+                Hp = data.Stat.Hp,
+                MaxHp = data.Stat.MaxHp,
+                Movement = data.Stat.Movement,
+                CombatStats = data.Stat.CombatStats.ToDictionary(
+                    kv => kv.Key,
+                    kv => new CombatStat
+                    {
+                        Type = kv.Value.Type,
+                        Range = kv.Value.Range,
+                        AttackCount = kv.Value.AttackCount,
+                        Value = kv.Value.Value
+                    }
+                ) ?? []
+            };
+        }
     }
 }
