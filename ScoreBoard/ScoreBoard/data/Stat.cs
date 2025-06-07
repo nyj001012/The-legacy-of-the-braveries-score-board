e742cdb35c59d18330ca2c221eb63ea5b76b37fe
nyj001012 <nyj001012@gmail.com>
2025-06-08 08:57:51 +0900

    feat: shield 필드 추가. 기본값 0

--- ScoreBoard/ScoreBoard/data/Stat.cs
+++ ScoreBoard/ScoreBoard/data/Stat.cs
@@ -11,6 +11,7 @@ namespace ScoreBoard.data
     {
         public ushort Hp { get; set; } // 체력 (예: 10, 20 등)
         public ushort MaxHp { get; set; } // 최대 체력 (예: 10, 20 등)
+        public ushort Shield { get; set; } = 0; // 방어막 (예: 5, 10 등)
         public ushort Movement { get; set; } // 이동 가능 거리 (예: 1, 2, 3 등)
         public ushort? SpellPower { get; set; } // 주문력 (예: 10, 20 등) - 선택적 속성
         public ushort? Wisdom { get; set; } // 지혜 = 카드 뽑는 횟수 (예: 1, 2, 3 등) - 선택적 속성
