9bd78844eb90b8070fc39938abd28b1cf243f45e
nyj001012 <nyj001012@gmail.com>
2025-06-08 09:48:17 +0900

    feat: organisation 새 객체가 아닌 이벤트 등록한 객체로 변경

--- ScoreBoard/ScoreBoard/forms/MainForm.cs
+++ ScoreBoard/ScoreBoard/forms/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -57,7 +58,7 @@ namespace ScoreBoard.forms
                     var (characters, monsters) = data;
                     ShowControl(new ScoreBoardControl(characters, monsters)); // 점수판 컨트롤 표시
                 };
-                ShowControl(new OrganisationControl()); // 부대 편성 컨트롤 표시
+                ShowControl(organisationControl); // 부대 편성 컨트롤 표시
             };
 
             ShowControl(mainControl);
