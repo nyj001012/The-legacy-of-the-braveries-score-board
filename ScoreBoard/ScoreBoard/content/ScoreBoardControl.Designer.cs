c8c67921328fe72755089d33164270e8e51dc6b0
nyj001012 <nyj001012@gmail.com>
2025-06-08 11:05:59 +0900

    feat: 이름, 강화수치, 상태이상, 장비 레이아웃 설정

--- ScoreBoard/ScoreBoard/content/ScoreBoardControl.Designer.cs
+++ ScoreBoard/ScoreBoard/content/ScoreBoardControl.Designer.cs
@@ -30,28 +30,37 @@
         {
             playerContainer = new ScoreBoard.controls.DoubleBufferedPanel();
             playerList = new ScoreBoard.controls.CustomFlowLayoutPanel();
-            playerScrollBar = new ReaLTaiizor.Controls.CyberScrollBar();
             pn1P = new ScoreBoard.controls.DoubleBufferedPanel();
-            pn2P = new ScoreBoard.controls.DoubleBufferedPanel();
-            pn3P = new ScoreBoard.controls.DoubleBufferedPanel();
-            pn4P = new ScoreBoard.controls.DoubleBufferedPanel();
+            pn1PInform = new ScoreBoard.controls.DoubleBufferedPanel();
+            fpn1P = new ScoreBoard.controls.CustomFlowLayoutPanel();
+            fpn1PStatus = new ScoreBoard.controls.CustomFlowLayoutPanel();
+            fpn1PArtifact = new ScoreBoard.controls.CustomFlowLayoutPanel();
+            pb1PLv = new PictureBox();
+            lbl1PName = new ScoreBoard.controls.GradientLabel();
             doubleBufferedPanel1 = new ScoreBoard.controls.DoubleBufferedPanel();
             lbl1P = new ScoreBoard.controls.GradientLabel();
-            lbl2P = new ScoreBoard.controls.GradientLabel();
+            pn2P = new ScoreBoard.controls.DoubleBufferedPanel();
             doubleBufferedPanel2 = new ScoreBoard.controls.DoubleBufferedPanel();
-            gradientLabel2 = new ScoreBoard.controls.GradientLabel();
+            lbl2P = new ScoreBoard.controls.GradientLabel();
+            pn3P = new ScoreBoard.controls.DoubleBufferedPanel();
             doubleBufferedPanel3 = new ScoreBoard.controls.DoubleBufferedPanel();
-            gradientLabel3 = new ScoreBoard.controls.GradientLabel();
+            gradientLabel2 = new ScoreBoard.controls.GradientLabel();
+            pn4P = new ScoreBoard.controls.DoubleBufferedPanel();
             doubleBufferedPanel4 = new ScoreBoard.controls.DoubleBufferedPanel();
+            gradientLabel3 = new ScoreBoard.controls.GradientLabel();
+            playerScrollBar = new ReaLTaiizor.Controls.CyberScrollBar();
             playerContainer.SuspendLayout();
             playerList.SuspendLayout();
             pn1P.SuspendLayout();
-            pn2P.SuspendLayout();
-            pn3P.SuspendLayout();
-            pn4P.SuspendLayout();
+            pn1PInform.SuspendLayout();
+            fpn1P.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pb1PLv).BeginInit();
             doubleBufferedPanel1.SuspendLayout();
+            pn2P.SuspendLayout();
             doubleBufferedPanel2.SuspendLayout();
+            pn3P.SuspendLayout();
             doubleBufferedPanel3.SuspendLayout();
+            pn4P.SuspendLayout();
             doubleBufferedPanel4.SuspendLayout();
             SuspendLayout();
             // 
@@ -78,6 +87,202 @@
             playerList.Padding = new Padding(15);
             playerList.Size = new Size(600, 980);
             playerList.TabIndex = 1;
+            // 
+            // pn1P
+            // 
+            pn1P.Controls.Add(pn1PInform);
+            pn1P.Controls.Add(doubleBufferedPanel1);
+            pn1P.Location = new Point(30, 30);
+            pn1P.Margin = new Padding(15, 15, 0, 15);
+            pn1P.Name = "pn1P";
+            pn1P.Size = new Size(538, 245);
+            pn1P.TabIndex = 0;
+            // 
+            // pn1PInform
+            // 
+            pn1PInform.Controls.Add(fpn1P);
+            pn1PInform.Controls.Add(pb1PLv);
+            pn1PInform.Controls.Add(lbl1PName);
+            pn1PInform.Dock = DockStyle.Right;
+            pn1PInform.Location = new Point(89, 0);
+            pn1PInform.Name = "pn1PInform";
+            pn1PInform.Size = new Size(449, 245);
+            pn1PInform.TabIndex = 1;
+            // 
+            // fpn1P
+            // 
+            fpn1P.BorderColor = Color.Transparent;
+            fpn1P.BorderThickness = 0;
+            fpn1P.Controls.Add(fpn1PStatus);
+            fpn1P.Controls.Add(fpn1PArtifact);
+            fpn1P.Location = new Point(0, 61);
+            fpn1P.Margin = new Padding(0);
+            fpn1P.Name = "fpn1P";
+            fpn1P.Size = new Size(449, 52);
+            fpn1P.TabIndex = 2;
+            // 
+            // fpn1PStatus
+            // 
+            fpn1PStatus.BorderColor = Color.Transparent;
+            fpn1PStatus.BorderThickness = 0;
+            fpn1PStatus.Location = new Point(0, 0);
+            fpn1PStatus.Margin = new Padding(0, 0, 5, 0);
+            fpn1PStatus.Name = "fpn1PStatus";
+            fpn1PStatus.Padding = new Padding(0, 0, 5, 0);
+            fpn1PStatus.Size = new Size(221, 52);
+            fpn1PStatus.TabIndex = 0;
+            // 
+            // fpn1PArtifact
+            // 
+            fpn1PArtifact.BorderColor = Color.Transparent;
+            fpn1PArtifact.BorderThickness = 0;
+            fpn1PArtifact.Location = new Point(226, 0);
+            fpn1PArtifact.Margin = new Padding(0);
+            fpn1PArtifact.Name = "fpn1PArtifact";
+            fpn1PArtifact.Padding = new Padding(0, 0, 5, 0);
+            fpn1PArtifact.Size = new Size(156, 52);
+            fpn1PArtifact.TabIndex = 1;
+            // 
+            // pb1PLv
+            // 
+            pb1PLv.Location = new Point(391, 3);
+            pb1PLv.Name = "pb1PLv";
+            pb1PLv.Size = new Size(52, 52);
+            pb1PLv.TabIndex = 1;
+            pb1PLv.TabStop = false;
+            // 
+            // lbl1PName
+            // 
+            lbl1PName.BackColor = Color.Transparent;
+            lbl1PName.Font = new Font("Danjo-bold", 23.9999962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl1PName.GradientEnd = Color.FromArgb(72, 144, 170);
+            lbl1PName.GradientStart = Color.FromArgb(221, 237, 240);
+            lbl1PName.Location = new Point(3, 3);
+            lbl1PName.Name = "lbl1PName";
+            lbl1PName.RightToLeft = RightToLeft.No;
+            lbl1PName.Size = new Size(382, 52);
+            lbl1PName.TabIndex = 0;
+            lbl1PName.Text = "예시샘플입니다.";
+            lbl1PName.TextAlign = ContentAlignment.BottomLeft;
+            // 
+            // doubleBufferedPanel1
+            // 
+            doubleBufferedPanel1.Controls.Add(lbl1P);
+            doubleBufferedPanel1.Location = new Point(0, 0);
+            doubleBufferedPanel1.Margin = new Padding(0);
+            doubleBufferedPanel1.Name = "doubleBufferedPanel1";
+            doubleBufferedPanel1.Size = new Size(80, 245);
+            doubleBufferedPanel1.TabIndex = 0;
+            // 
+            // lbl1P
+            // 
+            lbl1P.BackColor = Color.Transparent;
+            lbl1P.Dock = DockStyle.Fill;
+            lbl1P.Font = new Font("Danjo-bold", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl1P.GradientEnd = Color.FromArgb(107, 89, 50);
+            lbl1P.GradientStart = Color.FromArgb(209, 162, 97);
+            lbl1P.Location = new Point(0, 0);
+            lbl1P.Name = "lbl1P";
+            lbl1P.Size = new Size(80, 245);
+            lbl1P.TabIndex = 0;
+            lbl1P.Text = "1P";
+            lbl1P.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pn2P
+            // 
+            pn2P.Controls.Add(doubleBufferedPanel2);
+            pn2P.Location = new Point(30, 315);
+            pn2P.Margin = new Padding(15, 25, 3, 3);
+            pn2P.Name = "pn2P";
+            pn2P.Size = new Size(505, 200);
+            pn2P.TabIndex = 1;
+            // 
+            // doubleBufferedPanel2
+            // 
+            doubleBufferedPanel2.Controls.Add(lbl2P);
+            doubleBufferedPanel2.Location = new Point(0, 0);
+            doubleBufferedPanel2.Margin = new Padding(0);
+            doubleBufferedPanel2.Name = "doubleBufferedPanel2";
+            doubleBufferedPanel2.Size = new Size(80, 200);
+            doubleBufferedPanel2.TabIndex = 1;
+            // 
+            // lbl2P
+            // 
+            lbl2P.BackColor = Color.Transparent;
+            lbl2P.Dock = DockStyle.Fill;
+            lbl2P.Font = new Font("Danjo-bold", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl2P.GradientEnd = Color.FromArgb(107, 89, 50);
+            lbl2P.GradientStart = Color.FromArgb(209, 162, 97);
+            lbl2P.Location = new Point(0, 0);
+            lbl2P.Name = "lbl2P";
+            lbl2P.Size = new Size(80, 200);
+            lbl2P.TabIndex = 0;
+            lbl2P.Text = "2P";
+            lbl2P.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pn3P
+            // 
+            pn3P.Controls.Add(doubleBufferedPanel3);
+            pn3P.Location = new Point(30, 543);
+            pn3P.Margin = new Padding(15, 25, 3, 3);
+            pn3P.Name = "pn3P";
+            pn3P.Size = new Size(505, 200);
+            pn3P.TabIndex = 2;
+            // 
+            // doubleBufferedPanel3
+            // 
+            doubleBufferedPanel3.Controls.Add(gradientLabel2);
+            doubleBufferedPanel3.Location = new Point(0, 0);
+            doubleBufferedPanel3.Margin = new Padding(0);
+            doubleBufferedPanel3.Name = "doubleBufferedPanel3";
+            doubleBufferedPanel3.Size = new Size(80, 200);
+            doubleBufferedPanel3.TabIndex = 2;
+            // 
+            // gradientLabel2
+            // 
+            gradientLabel2.BackColor = Color.Transparent;
+            gradientLabel2.Dock = DockStyle.Fill;
+            gradientLabel2.Font = new Font("Danjo-bold", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            gradientLabel2.GradientEnd = Color.FromArgb(107, 89, 50);
+            gradientLabel2.GradientStart = Color.FromArgb(209, 162, 97);
+            gradientLabel2.Location = new Point(0, 0);
+            gradientLabel2.Name = "gradientLabel2";
+            gradientLabel2.Size = new Size(80, 200);
+            gradientLabel2.TabIndex = 0;
+            gradientLabel2.Text = "3P";
+            gradientLabel2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pn4P
+            // 
+            pn4P.Controls.Add(doubleBufferedPanel4);
+            pn4P.Location = new Point(30, 771);
+            pn4P.Margin = new Padding(15, 25, 3, 3);
+            pn4P.Name = "pn4P";
+            pn4P.Size = new Size(505, 200);
+            pn4P.TabIndex = 3;
+            // 
+            // doubleBufferedPanel4
+            // 
+            doubleBufferedPanel4.Controls.Add(gradientLabel3);
+            doubleBufferedPanel4.Location = new Point(0, 0);
+            doubleBufferedPanel4.Margin = new Padding(0);
+            doubleBufferedPanel4.Name = "doubleBufferedPanel4";
+            doubleBufferedPanel4.Size = new Size(80, 200);
+            doubleBufferedPanel4.TabIndex = 2;
+            // 
+            // gradientLabel3
+            // 
+            gradientLabel3.BackColor = Color.Transparent;
+            gradientLabel3.Dock = DockStyle.Fill;
+            gradientLabel3.Font = new Font("Danjo-bold", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            gradientLabel3.GradientEnd = Color.FromArgb(107, 89, 50);
+            gradientLabel3.GradientStart = Color.FromArgb(209, 162, 97);
+            gradientLabel3.Location = new Point(0, 0);
+            gradientLabel3.Name = "gradientLabel3";
+            gradientLabel3.Size = new Size(80, 200);
+            gradientLabel3.TabIndex = 0;
+            gradientLabel3.Text = "4P";
+            gradientLabel3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // playerScrollBar
             // 
@@ -121,130 +326,6 @@
             playerScrollBar.Timer_RGB = 300;
             playerScrollBar.Value = 0;
             // 
-            // pn1P
-            // 
-            pn1P.Controls.Add(doubleBufferedPanel1);
-            pn1P.Location = new Point(30, 30);
-            pn1P.Margin = new Padding(15, 15, 0, 15);
-            pn1P.Name = "pn1P";
-            pn1P.Size = new Size(538, 245);
-            pn1P.TabIndex = 0;
-            // 
-            // pn2P
-            // 
-            pn2P.Controls.Add(doubleBufferedPanel2);
-            pn2P.Location = new Point(30, 315);
-            pn2P.Margin = new Padding(15, 25, 3, 3);
-            pn2P.Name = "pn2P";
-            pn2P.Size = new Size(505, 200);
-            pn2P.TabIndex = 1;
-            // 
-            // pn3P
-            // 
-            pn3P.Controls.Add(doubleBufferedPanel3);
-            pn3P.Location = new Point(30, 543);
-            pn3P.Margin = new Padding(15, 25, 3, 3);
-            pn3P.Name = "pn3P";
-            pn3P.Size = new Size(505, 200);
-            pn3P.TabIndex = 2;
-            // 
-            // pn4P
-            // 
-            pn4P.Controls.Add(doubleBufferedPanel4);
-            pn4P.Location = new Point(30, 771);
-            pn4P.Margin = new Padding(15, 25, 3, 3);
-            pn4P.Name = "pn4P";
-            pn4P.Size = new Size(505, 200);
-            pn4P.TabIndex = 3;
-            // 
-            // doubleBufferedPanel1
-            // 
-            doubleBufferedPanel1.Controls.Add(lbl1P);
-            doubleBufferedPanel1.Location = new Point(0, 0);
-            doubleBufferedPanel1.Margin = new Padding(0);
-            doubleBufferedPanel1.Name = "doubleBufferedPanel1";
-            doubleBufferedPanel1.Size = new Size(80, 245);
-            doubleBufferedPanel1.TabIndex = 0;
-            // 
-            // lbl1P
-            // 
-            lbl1P.BackColor = Color.Transparent;
-            lbl1P.Dock = DockStyle.Fill;
-            lbl1P.Font = new Font("Danjo-bold", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbl1P.GradientEnd = Color.FromArgb(107, 89, 50);
-            lbl1P.GradientStart = Color.FromArgb(209, 162, 97);
-            lbl1P.Location = new Point(0, 0);
-            lbl1P.Name = "lbl1P";
-            lbl1P.Size = new Size(80, 245);
-            lbl1P.TabIndex = 0;
-            lbl1P.Text = "1P";
-            // 
-            // lbl2P
-            // 
-            lbl2P.BackColor = Color.Transparent;
-            lbl2P.Dock = DockStyle.Fill;
-            lbl2P.Font = new Font("Danjo-bold", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lbl2P.GradientEnd = Color.FromArgb(107, 89, 50);
-            lbl2P.GradientStart = Color.FromArgb(209, 162, 97);
-            lbl2P.Location = new Point(0, 0);
-            lbl2P.Name = "lbl2P";
-            lbl2P.Size = new Size(80, 200);
-            lbl2P.TabIndex = 0;
-            lbl2P.Text = "2P";
-            // 
-            // doubleBufferedPanel2
-            // 
-            doubleBufferedPanel2.Controls.Add(lbl2P);
-            doubleBufferedPanel2.Location = new Point(0, 0);
-            doubleBufferedPanel2.Margin = new Padding(0);
-            doubleBufferedPanel2.Name = "doubleBufferedPanel2";
-            doubleBufferedPanel2.Size = new Size(80, 200);
-            doubleBufferedPanel2.TabIndex = 1;
-            // 
-            // gradientLabel2
-            // 
-            gradientLabel2.BackColor = Color.Transparent;
-            gradientLabel2.Dock = DockStyle.Fill;
-            gradientLabel2.Font = new Font("Danjo-bold", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            gradientLabel2.GradientEnd = Color.FromArgb(107, 89, 50);
-            gradientLabel2.GradientStart = Color.FromArgb(209, 162, 97);
-            gradientLabel2.Location = new Point(0, 0);
-            gradientLabel2.Name = "gradientLabel2";
-            gradientLabel2.Size = new Size(80, 200);
-            gradientLabel2.TabIndex = 0;
-            gradientLabel2.Text = "3P";
-            // 
-            // doubleBufferedPanel3
-            // 
-            doubleBufferedPanel3.Controls.Add(gradientLabel2);
-            doubleBufferedPanel3.Location = new Point(0, 0);
-            doubleBufferedPanel3.Margin = new Padding(0);
-            doubleBufferedPanel3.Name = "doubleBufferedPanel3";
-            doubleBufferedPanel3.Size = new Size(80, 200);
-            doubleBufferedPanel3.TabIndex = 2;
-            // 
-            // gradientLabel3
-            // 
-            gradientLabel3.BackColor = Color.Transparent;
-            gradientLabel3.Dock = DockStyle.Fill;
-            gradientLabel3.Font = new Font("Danjo-bold", 27.7499962F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            gradientLabel3.GradientEnd = Color.FromArgb(107, 89, 50);
-            gradientLabel3.GradientStart = Color.FromArgb(209, 162, 97);
-            gradientLabel3.Location = new Point(0, 0);
-            gradientLabel3.Name = "gradientLabel3";
-            gradientLabel3.Size = new Size(80, 200);
-            gradientLabel3.TabIndex = 0;
-            gradientLabel3.Text = "4P";
-            // 
-            // doubleBufferedPanel4
-            // 
-            doubleBufferedPanel4.Controls.Add(gradientLabel3);
-            doubleBufferedPanel4.Location = new Point(0, 0);
-            doubleBufferedPanel4.Margin = new Padding(0);
-            doubleBufferedPanel4.Name = "doubleBufferedPanel4";
-            doubleBufferedPanel4.Size = new Size(80, 200);
-            doubleBufferedPanel4.TabIndex = 2;
-            // 
             // ScoreBoardControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -257,12 +338,15 @@
             playerContainer.ResumeLayout(false);
             playerList.ResumeLayout(false);
             pn1P.ResumeLayout(false);
-            pn2P.ResumeLayout(false);
-            pn3P.ResumeLayout(false);
-            pn4P.ResumeLayout(false);
+            pn1PInform.ResumeLayout(false);
+            fpn1P.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pb1PLv).EndInit();
             doubleBufferedPanel1.ResumeLayout(false);
+            pn2P.ResumeLayout(false);
             doubleBufferedPanel2.ResumeLayout(false);
+            pn3P.ResumeLayout(false);
             doubleBufferedPanel3.ResumeLayout(false);
+            pn4P.ResumeLayout(false);
             doubleBufferedPanel4.ResumeLayout(false);
             ResumeLayout(false);
         }
@@ -284,5 +368,11 @@
         private controls.GradientLabel gradientLabel2;
         private controls.DoubleBufferedPanel doubleBufferedPanel4;
         private controls.GradientLabel gradientLabel3;
+        private controls.DoubleBufferedPanel pn1PInform;
+        private controls.GradientLabel lbl1PName;
+        private PictureBox pb1PLv;
+        private controls.CustomFlowLayoutPanel fpn1P;
+        private controls.CustomFlowLayoutPanel fpn1PStatus;
+        private controls.CustomFlowLayoutPanel fpn1PArtifact;
     }
 }
--- ScoreBoard/ScoreBoard/controls/GradientLabel.cs
+++ ScoreBoard/ScoreBoard/controls/GradientLabel.cs
@@ -8,6 +8,17 @@ namespace ScoreBoard.controls
     {
         public Color GradientStart { get; set; } = ColorTranslator.FromHtml("#D1A261");
         public Color GradientEnd { get; set; } = ColorTranslator.FromHtml("#6B5932");
+        private ContentAlignment textAlign = ContentAlignment.MiddleCenter;
+
+        public ContentAlignment TextAlign
+        {
+            get => textAlign;
+            set
+            {
+                textAlign = value;
+                Invalidate(); // 값 바뀌면 다시 그리기
+            }
+        }
 
         public GradientLabel()
         {
@@ -29,14 +40,64 @@ namespace ScoreBoard.controls
                 GradientEnd,
                 LinearGradientMode.Vertical))
             {
+                GetAlignments(this.TextAlign, out var hAlign, out var vAlign);
+
                 StringFormat format = new StringFormat
                 {
-                    Alignment = StringAlignment.Center,
-                    LineAlignment = StringAlignment.Center
+                    Alignment = hAlign,
+                    LineAlignment = vAlign
                 };
 
                 e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
                 e.Graphics.DrawString(this.Text, this.Font, brush, this.ClientRectangle, format);
+            }
+        }
+
+
+        private void GetAlignments(ContentAlignment align, out StringAlignment horizontal, out StringAlignment vertical)
+        {
+            switch (align)
+            {
+                case ContentAlignment.TopLeft:
+                    horizontal = StringAlignment.Near;
+                    vertical = StringAlignment.Near;
+                    break;
+                case ContentAlignment.TopCenter:
+                    horizontal = StringAlignment.Center;
+                    vertical = StringAlignment.Near;
+                    break;
+                case ContentAlignment.TopRight:
+                    horizontal = StringAlignment.Far;
+                    vertical = StringAlignment.Near;
+                    break;
+                case ContentAlignment.MiddleLeft:
+                    horizontal = StringAlignment.Near;
+                    vertical = StringAlignment.Center;
+                    break;
+                case ContentAlignment.MiddleCenter:
+                    horizontal = StringAlignment.Center;
+                    vertical = StringAlignment.Center;
+                    break;
+                case ContentAlignment.MiddleRight:
+                    horizontal = StringAlignment.Far;
+                    vertical = StringAlignment.Center;
+                    break;
+                case ContentAlignment.BottomLeft:
+                    horizontal = StringAlignment.Near;
+                    vertical = StringAlignment.Far;
+                    break;
+                case ContentAlignment.BottomCenter:
+                    horizontal = StringAlignment.Center;
+                    vertical = StringAlignment.Far;
+                    break;
+                case ContentAlignment.BottomRight:
+                    horizontal = StringAlignment.Far;
+                    vertical = StringAlignment.Far;
+                    break;
+                default:
+                    horizontal = StringAlignment.Center;
+                    vertical = StringAlignment.Center;
+                    break;
             }
         }
     }
