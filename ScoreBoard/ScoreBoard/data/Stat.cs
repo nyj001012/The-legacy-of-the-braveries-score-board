aa716c8eb0c07449cbcfacfeb2c2d4b945cd3d48
nyj001012 <nyj001012@gmail.com>
2025-06-16 12:29:48 +0900

    feat: 상태이상을 나타내는 클래스 추가

--- ScoreBoard/ScoreBoard/data/Stat.cs
+++ ScoreBoard/ScoreBoard/data/Stat.cs
@@ -15,6 +15,7 @@ namespace ScoreBoard.data
         public ushort Movement { get; set; } // 이동 가능 거리 (예: 1, 2, 3 등)
         public ushort? SpellPower { get; set; } // 주문력 (예: 10, 20 등) - 선택적 속성
         public ushort? Wisdom { get; set; } // 지혜 = 카드 뽑는 횟수 (예: 1, 2, 3 등) - 선택적 속성
+        public List<StatusEffect> StatusEffects { get; set; } = []; // 상태이상 정보 배열 (예: 중독, 기절 등)
 
         [JsonPropertyName("combatstat")]
         public Dictionary<string, CombatStat> CombatStats { get; set; } = []; // 공격 타입별 공격력 정보 배열 (예: 물리(atk), 마법(sp) 등)
---
+++ ScoreBoard/ScoreBoard/data/StatusEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data
+{
+    public class StatusEffect
+    {
+        public StatusEffectType Type { get; set; } // 상태이상의 종류 (예: Poison, Stun 등)
+        public int Duration { get; set; } // 상태이상의 지속 시간 (예: 2, 3 등)
+        public bool IsInfinite => Duration < 0; // 무한 지속 여부 (예: true, false)
+    }
+}
