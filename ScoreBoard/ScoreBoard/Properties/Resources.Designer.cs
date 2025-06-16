d27edd55d577c6c8da7dd0514d722dd240c9fcf3
nyj001012 <nyj001012@gmail.com>
2025-06-16 12:29:40 +0900

    feat: 레벨을 나타내는 이미지 추가

--- ScoreBoard/ScoreBoard/Properties/Resources.Designer.cs
+++ ScoreBoard/ScoreBoard/Properties/Resources.Designer.cs
@@ -223,6 +223,36 @@ namespace ScoreBoard.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap Lv1 {
+            get {
+                object obj = ResourceManager.GetObject("Lv1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap Lv2 {
+            get {
+                object obj = ResourceManager.GetObject("Lv2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Bitmap Lv3 {
+            get {
+                object obj = ResourceManager.GetObject("Lv3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap 참전_버튼 {
             get {
                 object obj = ResourceManager.GetObject("참전 버튼", resourceCulture);
