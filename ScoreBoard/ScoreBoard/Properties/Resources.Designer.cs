c01d9f5dd5d57f200f81d474a1a709aababfc151
nyj001012 <nyj001012@gmail.com>
2025-06-16 12:52:01 +0900

    feat: 빈 슬롯 resource명 수정

--- ScoreBoard/ScoreBoard/Properties/Resources.Designer.cs
+++ ScoreBoard/ScoreBoard/Properties/Resources.Designer.cs
@@ -163,6 +163,16 @@ namespace ScoreBoard.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap EmptyArtifactSlot {
+            get {
+                object obj = ResourceManager.GetObject("EmptyArtifactSlot", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap ImgBackground {
             get {
                 object obj = ResourceManager.GetObject("ImgBackground", resourceCulture);
