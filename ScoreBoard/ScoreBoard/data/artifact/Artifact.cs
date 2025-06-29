4e0c07889bcbebb8d871a635db9646cf917c7329
nyj001012 <nyj001012@gmail.com>
2025-06-29 14:18:26 +0900

    feat: 네임스페이스 변경

---
+++ ScoreBoard/ScoreBoard/data/artifact/Artifact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data.artifact
+{
+    public class Artifact
+    {
+        private enum ArtifactType
+        {
+            Weapon = 0,
+            Armour = 1,
+            Accessory = 2
+        }
+    }
+}
