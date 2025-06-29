9d1405bd4147304d5463a39e35f474137ada8698
nyj001012 <nyj001012@gmail.com>
2025-06-29 15:42:13 +0900

    feat: 희귀도 추가

--- ScoreBoard/ScoreBoard/data/artifact/Artifact.cs
+++ ScoreBoard/ScoreBoard/data/artifact/Artifact.cs
@@ -9,13 +9,23 @@ namespace ScoreBoard.data.artifact
 {
     public class Artifact
     {
+        public string Id { get; set; } = string.Empty; // 유물 Id: [타입]_[번호]_[이름] (예: "0_01_DingDingSword")
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public ArtifactType Type { get; set; } = ArtifactType.Weapon;
+        public ArtifactRarity Rarity { get; set; } = ArtifactRarity.Normal; // 유물의 희귀도 (Normal, Rare, Legendary)
 
         public virtual void Equip(CorpsMember member) { }
         public virtual void Unequip(CorpsMember member) { }
 
-        private enum ArtifactType
+        public enum ArtifactRarity
+        {
+            Normal = 0,
+            Rare = 1,
+            Legendary = 2
+        }
+
+        public enum ArtifactType
         {
             Weapon = 0,
             Armour = 1,
