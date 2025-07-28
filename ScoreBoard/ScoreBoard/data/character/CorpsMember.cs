42078fd21475439482408bfacf8ce16c60173c18
nyj001012 <nyj001012@gmail.com>
2025-07-28 22:55:53 +0900

    feat: 미리 maxSlot만큼 유물 슬롯 생성

--- ScoreBoard/ScoreBoard/data/character/CorpsMember.cs
+++ ScoreBoard/ScoreBoard/data/character/CorpsMember.cs
@@ -18,9 +18,14 @@ namespace ScoreBoard.data.character
         public Stat Stat { get; set; } = new(); // 멤버의 능력치 정보
         public List<PassiveSkill> Passives { get; set; } = []; // 멤버의 패시브 스킬 정보
         public List<ActiveSkill> Actives { get; set; } = []; // 멤버의 능력치 정보
-        public List<Artifact> ArtifactSlot { get; set; } = []; // 멤버의 유물 슬롯 정보 (예: 무기, 방어구 등)
+        public List<Artifact?> ArtifactSlot { get; set; } = []; // 멤버의 유물 슬롯 정보 (예: 무기, 방어구 등)
         public ushort MaxArtifactSlot { get; set; } = 3; // 최대 유물 슬롯 수 (예: 3개)
         public ushort Level { get; set; } = 0; // 멤버의 레벨 (예: 1, 2, 3 등)
         public Dictionary<ushort, bool> RequiredDiceValues { get; set; } = []; // 멤버가 행동하기 위해 필요한 주사위 값이 키, 치명타 여부가 값
+
+        public CorpsMember()
+        {
+            ArtifactSlot = [.. Enumerable.Repeat<Artifact?>(null,  MaxArtifactSlot)]; // 0: 무기, 1: 방어구, 2~3: 액세서리
+        }
     }
 }
