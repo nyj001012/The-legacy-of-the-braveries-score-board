b1e220459df73a0416a4891b8b1ed8b94071a475
nyj001012 <nyj001012@gmail.com>
2025-08-04 12:59:53 +0900

    feat: 날씨 클래스 추가

---
+++ ScoreBoard/ScoreBoard/data/weather/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data.weather
+{
+    internal class Weather
+    {
+        public WeatherType Type { get; set; } = WeatherType.Clear; // 날씨 타입
+        public int Duration { get; set; } = -1; // 날씨 지속 턴 수
+        public bool IsInfinite => Duration < 0; // 날씨가 무한으로 지속되는지 여부
+    }
+}
