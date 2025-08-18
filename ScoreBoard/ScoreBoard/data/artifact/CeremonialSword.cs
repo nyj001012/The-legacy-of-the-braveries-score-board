0c035f29b5ed3bf7f08a97ee9ac319cb301c7349
nyj001012 <nyj001012@gmail.com>
2025-08-18 18:58:23 +0900

    feat: 의장검 추가

---
+++ ScoreBoard/ScoreBoard/data/artifact/CeremonialSword.cs
@@ -0,0 +1,42 @@
+﻿using ScoreBoard.data.character;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data.artifact
+{
+    internal class CeremonialSword : Artifact
+    {
+        public CeremonialSword()
+        {
+        }
+
+        public override void Equip(CorpsMember member)
+        {
+            // 공격력 60 증가
+            if (member.Stat.CombatStats.TryGetValue("melee", out var melee))
+            {
+                melee.Value += 60;
+            }
+            if (member.Stat.CombatStats.TryGetValue("ranged", out var ranged))
+            {
+                ranged.Value += 60;
+            }
+        }
+
+        public override void Unequip(CorpsMember member)
+        {
+            // 공격력 60 감소
+            if (member.Stat.CombatStats.TryGetValue("melee", out var melee))
+            {
+                melee.Value = (ushort)Math.Max(0, (int)melee.Value - 60);
+            }
+            else if (member.Stat.CombatStats.TryGetValue("ranged", out var ranged))
+            {
+                ranged.Value = (ushort)Math.Max(0, (int)ranged.Value - 60);
+            }
+        }
+    }
+}
