1208a7d23701fbcdfabba1d1ae992b29352f8089
nyj001012 <nyj001012@gmail.com>
2025-08-19 15:53:24 +0900

    feat: 탱커인지 확인하는 메서드 추가

--- ScoreBoard/ScoreBoard/utils/Validator.cs
+++ ScoreBoard/ScoreBoard/utils/Validator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScoreBoard.data.character;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,21 @@ namespace ScoreBoard.utils
         public static T ValidateNull<T>(T? value, string name) where T : class
         {
             return value ?? throw new ArgumentNullException(name, $"{name} cannot be null.");
+        }
+
+        /*
+         * IsTanker<T>(T member)
+         * - T는 CorpsMember의 자식 클래스여야 함
+         * - member가 Alber, Karma, Seokbungdius, Petrus, Joshua, Griffin 중 하나인지 확인
+         */
+        public static bool IsTanker<T>(T member) where T : class
+        {
+            return member is Alber
+                || member is Karma
+                || member is Seokbungdius
+                || member is Petrus
+                || member is Joshua
+                || member is Griffin;
         }
     }
 }
