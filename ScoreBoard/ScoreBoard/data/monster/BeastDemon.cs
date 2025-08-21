607f631db3738aeb5d645fdd68d871578c78b52c
nyj001012 <nyj001012@gmail.com>
2025-08-21 13:28:03 +0900

    feat: 짐승악마 추가

---
+++ ScoreBoard/ScoreBoard/data/monster/BeastDemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data.monster
+{
+    internal class BeastDemon : Monster
+    {
+        public BeastDemon(string id, ushort spawnTurn) : base()
+        {
+            InitialiseNormalElite(id, spawnTurn);
+        }
+    }
+}
