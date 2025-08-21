d11f67769b70df791cea5a43b8ec1004dfabd60d
nyj001012 <nyj001012@gmail.com>
2025-08-21 13:15:32 +0900

    feat: 임프 추가

--- ScoreBoard/ScoreBoard/content/OrganisationControl.cs
+++ ScoreBoard/ScoreBoard/content/OrganisationControl.cs
@@ -100,7 +100,7 @@ namespace ScoreBoard.content
                     Monster monster = id switch
                     {
                         "2_01_white_soldier" => new Slime(id, 0),// 스폰 턴은 0으로 설정
-                        "2_02_black_knight" => new BlackKnight(id, 0),
+                        "2_02_black_knight" => new Imp(id, 0),
                         _ => throw new ArgumentException($"알 수 없는 몬스터 ID: {id}"),
                     };
                     monster.Count = count;
