10a6205d85b731f325b65f4dab2904f335ff6fec
nyj001012 <nyj001012@gmail.com>
2025-09-09 15:28:21 +0900

    feat: 소환수의 기본 클래스 추가

---
+++ ScoreBoard/ScoreBoard/data/minion/Minion.cs
@@ -0,0 +1,22 @@
+﻿using ScoreBoard.data.skill;
+using ScoreBoard.data.stat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreBoard.data.minion
+{
+    internal class Minion
+    {
+        public string Id { get; set; } = string.Empty; // 소환수 ID: [캐릭터 ID]_[번호]_이름
+        public string Name { get; set; } = string.Empty; // 소환수 이름 (예: 햄부기 전차)
+        public Stat Stat { get; set; } = new(); // 소환수의 능력치 정보
+        public List<PassiveSkill> Passives { get; set; } = []; // 소환수의 패시브 스킬 정보
+        public List<ActiveSkill> Actives { get; set; } = []; // 소환수의 능력치 정보
+        public int SummonAvailableTurn { get; set; } = 0; // 소환 가능 턴 (예: 0턴 후 소환 가능)
+        public int SummonEndTurn { get; set; } = -1; // 소환 종료 턴 (예: 3턴 후 소환 종료, -1이면 죽기 전까지 소환 유지)
+        public bool IsSummonable { get; set; } = false; // 소환 가능 여부
+    }
+}
